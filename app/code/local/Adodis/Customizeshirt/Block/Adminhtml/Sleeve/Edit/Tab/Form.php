9e5654cf9b572a9ab4dbb29c6387dc849a185d74
Richard Valsalan <richard@adodis.com>
2013-11-11 17:10:49 +0530

    adding the sleeve edit form and the necessary action addition in the controller

---
+++ app/code/local/Adodis/Customizeshirt/Block/Adminhtml/Sleeve/Edit/Tab/Form.php
@@ -0,0 +1,103 @@
+<?php
+
+class Adodis_Customizeshirt_Block_Adminhtml_Sleeve_Edit_Tab_Form extends Mage_Adminhtml_Block_Widget_Form
+{
+  protected function _prepareForm()
+  {
+	$model = Mage::registry('sleeve_data');
+      $form = new Varien_Data_Form();
+      $this->setForm($form);
+      $fieldset = $form->addFieldset('customizeshirt_form', array('legend'=>Mage::helper('customizeshirt')->__('Item information')));
+     
+      $fieldset->addField('sleeve_title', 'text', array(
+          'label'     => Mage::helper('customizeshirt')->__('Sleeve Title'),
+          'class'     => 'required-entry',
+          'required'  => true,
+          'name'      => 'sleeve_title',
+      ));
+
+      $object = Mage::getModel('customizeshirt/sleeve')->load( $this->getRequest()->getParam('id') );
+		$note = false;
+		$img = '';
+		$fieldset->addField('imagethumb', 'hidden', array(
+	          	'name'        => 'imagethumb',
+	      	));
+
+		if( $object["sleeve_image"] ){
+	  		//$fieldset->addField('imagethumb', 'hidden', array(
+	         // 	'name'        => 'imagethumb',
+	      	//));
+
+			$config = Mage::getModel('customizeshirt/sleeve');
+			$image = Mage::getModel('customizeshirt/sleeve');
+			$image->setConfig($config);
+
+			$imageFile =  Mage::getBaseUrl('media').$object["sleeve_image"];
+			$strimg = $object["sleeve_image"];
+			$aryimg = explode("/",$strimg);
+			$imge = $aryimg[2].'/'.$aryimg[3].'/'.$aryimg[4];
+			$imge = ltrim(rtrim($imge));
+			If ($imageFile) {
+				$img = '<img src="' . $imageFile . '" border="0" width="150" height="150" align="center"/>';
+			}
+		}
+
+
+
+	  $fieldset->addField('sleeve_image', 'file', array(
+			'label'        => Mage::helper('customizeshirt')->__('Sleeve Image'),
+			'note'      => $note,
+			'name'        => 'filename',
+			'after_element_html' => $img,
+   		 ));
+		
+      $fieldset->addField('status', 'select', array(
+          'label'     => Mage::helper('customizeshirt')->__('Status'),
+          'name'      => 'status',
+          'values'    => array(
+              array(
+                  'value'     => 1,
+                  'label'     => Mage::helper('customizeshirt')->__('Enabled'),
+              ),
+
+              array(
+                  'value'     => 2,
+                  'label'     => Mage::helper('customizeshirt')->__('Disabled'),
+              ),
+          ),
+      ));
+
+	  $fieldset->addField('switchno', 'text', array(
+          'label'     => Mage::helper('customizeshirt')->__('Sleeve Switch no'),
+          'class'     => 'required-entry',
+          'required'  => true,
+          'name'      => 'switchno',
+      ));	  
+      
+	if (!Mage::app()->isSingleStoreMode()) {
+		$fieldset->addField('store_id', 'multiselect', 
+				array (
+						'name' => 'stores[]', 
+						'label' => Mage::helper('customizeshirt')->__('Store view'), 
+						'title' => Mage::helper('customizeshirt')->__('Store view'), 
+						'required' => true, 
+						'values' => Mage::getSingleton('adminhtml/system_store')->getStoreValuesForForm(false, true) ));
+	}
+	else {
+		$fieldset->addField('store_id', 'hidden', array (
+				'name' => 'stores[]', 
+				'value' => Mage::app()->getStore(true)->getId() ));
+		$model->setStoreId(Mage::app()->getStore(true)->getId());
+	} 
+      
+     
+      if ( Mage::getSingleton('adminhtml/session')->getSleeveData() )
+      {
+          $form->setValues(Mage::getSingleton('adminhtml/session')->getSleeveData());
+          Mage::getSingleton('adminhtml/session')->getSleeveData(null);
+      } elseif ( Mage::registry('sleeve_data') ) {
+          $form->setValues(Mage::registry('sleeve_data')->getData());
+      }
+      return parent::_prepareForm();
+  }
+}
